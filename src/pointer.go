ee9be5c49e676d9e6a22ff4b7fdf854ec57ef847
maria_lobillo <maria.lobillo.santos@gmail.com>
2015-11-03 20:09:59 +0100

    pointers : part 3

--- src/pointer.go
+++ src/pointer.go
@@ -7,5 +7,8 @@ func main() {
 	var message string = "Hello Go World!"
 	var greeting *string = &message
 
+	//variable and pointer is = "hi"
+	*greeting = "hi"
+
 	fmt.Println(message, *greeting)
 }
