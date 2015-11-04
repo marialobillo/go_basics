54a47facc5d0978838a0270458500d9239edf731
maria_lobillo <maria.lobillo.santos@gmail.com>
2015-11-04 14:54:24 +0100

    variadic functions

--- simple_go/functions/variadicfunctions.go
+++ simple_go/functions/variadicfunctions.go
@@ -8,14 +8,15 @@ type Salutation struct {
 }
 
 func Greet(salutation Salutation){
-  message, alternate := CreateMessage(salutation.name, salutation.greeting)
+  message, alternate := CreateMessage(salutation.name, salutation.greeting, "yo")
   fmt.Println(message)
   fmt.Println(alternate)
 }
 
 
-func CreateMessage(name, greeting string) (message string, alternate string) {
-  message = greeting + " " + name
+func CreateMessage(name string, greeting ...string) (message string, alternate string) {
+  fmt.Println(len(greeting))
+  message = greeting[1] + " " + name
   alternate = "HEY! " + name
   return
 }
