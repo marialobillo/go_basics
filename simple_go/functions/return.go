6bdd2ee022cf918e50ec68be542b2e9a72681493
maria_lobillo <maria.lobillo.santos@gmail.com>
2015-11-04 13:44:12 +0100

    functions with return value

---
+++ simple_go/functions/return.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+type Salutation struct {
+	name     string
+	greeting string
+}
+
+func Greet(salutation Salutation){
+  fmt.Println(CreateMessage(salutation.name, salutation.greeting))
+}
+
+func CreateMessage(name, greeting string) string {
+  return greeting + " " + name
+}
+
+func main() {
+	var s = Salutation{"Bob", "Hello"}
+
+  Greet(s)
+}
