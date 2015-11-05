e507149445622e241bd98fbc2211a037cefc843d
maria_lobillo <maria.lobillo.santos@gmail.com>
2015-11-05 10:36:43 +0100

    switch

---
+++ simple_go/branching/switch.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func GetPrefix(name string) (prefix string){
+  switch name {
+    case "Bob": prefix = "Mr "
+    case "Joe": prefix = "Dr"
+    case "Mary": prefix = "Mrs"
+    default: prefix = "Dude "
+
+  }
+  return
+}
+
+func Greet(name string, isFormal bool){
+
+  if prefix := GetPrefix(name); isFormal {
+    fmt.Println(prefix + " " + name)
+  }
+}
+
+func main(){
+
+  Greet("Mary", true)
+}
