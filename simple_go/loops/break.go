21991599445b09e9ed5434d8dd3e6383fcab38ef
maria_lobillo <maria.lobillo.santos@gmail.com>
2015-11-08 12:00:01 +0100

    break

---
+++ simple_go/loops/break.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func Greet(prefix)
+{
+	i := 0
+	for{
+		if i >= prefix{
+		   break
+		}
+		else{
+		   fmt.Println("Hello")
+		}
+		i++
+	}
+
+}
+
+
+func main(){
+
+     Greet(4)
+}
