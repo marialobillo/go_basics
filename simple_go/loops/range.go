e0affbf07583e9fd8077d3d8f29806efc9f21d5d
maria_lobillo <maria.lobillo.santos@gmail.com>
2015-11-09 09:06:46 +0100

    range loop in golfing

---
+++ simple_go/loops/range.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+
+
+
+func main(){
+
+    
+   a := [...]string{"a", "b", "c", "d"}
+    for i := range a {
+        fmt.Println("Array item", i, "is", a[i])
+    }
+}
+
