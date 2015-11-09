4a4adc9d4cec6af8257100ac7099fde1cad3e081
maria_lobillo <maria.lobillo.santos@gmail.com>
2015-11-09 08:44:28 +0100

    continue

---
+++ simple_go/loops/continue.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func Greet(prefix)
+{
+	i := 0
+	for{
+		if i >= prefix{
+		   break
+		}
+
+		if i % 2 == 0 {
+
+		   i++
+		   continue
+		}
+
+	}
+
+}
+
+
+func main(){
+
+     Greet(4)
+}
+
